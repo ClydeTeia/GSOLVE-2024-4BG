f977d9ac5f8368e02fb1b8a9fdfd796a250f5bf7
chdean-09 <chaddenard.andrada-19@cpu.edu.ph>
2024-03-23 01:53:12 +0800

    fixed member link share

--- src/components/classroom/teacher/withParams/displayMember.tsx
+++ src/components/classroom/teacher/withParams/displayMember.tsx
@@ -12,6 +12,8 @@ type Props = {
 const DisplayMember = ({ params, challengeData }: Props) => {
   console.log("On DisplayMember " + params);
 
+  console.log(challengeData, 'skka')
+
   const handleInviteClick = () => {
     const inviteText = `http://localhost:3000/classroom/join/${params}`;
     console.log(inviteText);
@@ -27,24 +29,16 @@ const DisplayMember = ({ params, challengeData }: Props) => {
 
   return (
     <div className="w-full h-full">
-      {!challengeData && (
-        <div className="flex flex-col items-center justify-center w-full h-full gap-5 p-10">
-          <p className="text-2xl font-bold ">Share this link to add them!</p>
-          <div className="flex items-center justify-center w-full h-10 p-5 border  border-gray-300 bg-gray-200 rounded-md text-sm">{`http://localhost:3000/classroom/join/${params}`}</div>
-          <Button
-            className="w-fit p-6 bg-blue-600 hover:bg-blue-500"
-            onClick={handleInviteClick}
-          >
-            Copy Link
-          </Button>
-        </div>
-      )}
-      {challengeData &&
-        challengeData.map((member) => (
-          <div key={member.id} className="flex flex-col w-full h-full">
-            <p>{member.name}</p>
-          </div>
-        ))}
+      <div className="flex flex-col items-center justify-center w-full h-full gap-5 p-10">
+        <p className="text-2xl font-bold ">Share this link to add them!</p>
+        <div className="flex items-center justify-center w-full h-10 p-5 border  border-gray-300 bg-gray-200 rounded-md text-sm">{`http://localhost:3000/classroom/join/${params}`}</div>
+        <Button
+          className="w-fit p-6 bg-blue-600 hover:bg-blue-500"
+          onClick={handleInviteClick}
+        >
+          Copy Link
+        </Button>
+      </div>
     </div>
   );
 };
