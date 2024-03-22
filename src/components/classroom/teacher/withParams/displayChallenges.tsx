43314615809442cca82c033a0d1b3216422bbc00
chdean-09 <chaddenard.andrada-19@cpu.edu.ph>
2024-03-23 00:08:14 +0800

    added redirect to challenge playground

--- src/components/classroom/teacher/withParams/displayChallenges.tsx
+++ src/components/classroom/teacher/withParams/displayChallenges.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { CreateChallengeButton } from "../../createChallenge";
+import Link from "next/link";
 
 type Props = {
   challengesData: ChallengeProp[] | null;
@@ -15,14 +16,24 @@ function DisplayChallenges({ link, challengesData }: Props) {
         <div className="text-center flex flex-col justify-center items-center w-full h-full gap-3">
           <div>No challenges available</div>
           <div>Create a challenge</div>
-          <CreateChallengeButton link={link}/>
+          <CreateChallengeButton link={link} />
         </div>
       )}
       {challengesData &&
         challengesData.map((challenge) => (
-          <div key={challenge.title}>
-            <div>{challenge.title}</div>
-          </div>
+          <Link key={challenge.title} href={{
+            pathname: `/playground/${challenge.title}`,
+            query: { challengeText: challenge.challengeText },
+          }}>
+            <div className="flex flex-col gap-3 w-full rounded-md shadow-md p-5 border border-[#77baac]">
+              <div>
+                <span className="font-bold">
+                  {challenge.title.toUpperCase() + ' '}
+                </span>
+                {challenge.challengeText}
+              </div>
+            </div>
+          </Link>
         ))}
     </div>
   );
